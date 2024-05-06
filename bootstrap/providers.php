59a08c41823e4b2a3b82b248076e378b470e6839
Alex Wulf <alex.f.wulf@gmail.com>
2024-05-06 12:35:47 +0200

    Fixed problems

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,5 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    Dnw\Foundation\Event\DnwModularEventServiceProvider::class,
     Dnw\Foundation\Providers\FoundationEventServiceProvider::class,
 ];
