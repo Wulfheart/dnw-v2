f24df1d65552fc88fd166e1fd1340acab41f0e5d
Alex Wulf <alex.f.wulf@gmail.com>
2024-05-15 23:05:36 +0200

    Game VO tested

---
+++ modules/game/tests/Unit/Domain/Game/ValueObject/Game/GameNameTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Dnw\Game\Tests\Unit\Domain\Game\ValueObject\Game;
+
+use Dnw\Game\Core\Domain\Game\ValueObject\Game\GameName;
+use InvalidArgumentException;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(GameName::class)]
+class GameNameTest extends TestCase
+{
+    public function test_needs_at_least_3_characters(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        GameName::fromString('ab');
+    }
+
+    public function test_has_at_most_50_characters(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        GameName::fromString(str_repeat('a', 51));
+    }
+
+    public function test_3_characters_is_valid(): void
+    {
+        $gameName = GameName::fromString('abc');
+        $this->assertEquals('abc', (string) $gameName);
+    }
+
+    public function test_50_characters_is_valid(): void
+    {
+        $gameName = GameName::fromString(str_repeat('a', 50));
+        $this->assertEquals(str_repeat('a', 50), (string) $gameName);
+    }
+}
