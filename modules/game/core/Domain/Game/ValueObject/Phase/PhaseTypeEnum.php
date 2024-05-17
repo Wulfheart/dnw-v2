e04f5645d0dc25af58f97d1bf523fa98dbb96584
Alex Wulf <alex.f.wulf@gmail.com>
2024-05-17 18:15:49 +0200

    PhaseTypeEnum fixed

--- modules/game/core/Domain/Game/ValueObject/Phase/PhaseTypeEnum.php
+++ modules/game/core/Domain/Game/ValueObject/Phase/PhaseTypeEnum.php
@@ -5,6 +5,8 @@ namespace Dnw\Game\Core\Domain\Game\ValueObject\Phase;
 enum PhaseTypeEnum: string
 {
     case MOVEMENT = 'M';
+    case RETREAT = 'R';
+    case ADJUSTMENT = 'A';
+    case NON_PLAYING = '-';
 
-    // TODO: Cover more cases
 }
