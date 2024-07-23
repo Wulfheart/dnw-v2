c9088d66ca5c2d29e576edec066b3ac20cc791b4
Alex Wulf <alex.f.wulf@gmail.com>
2024-07-23 23:52:46 +0200

    Resources moved

--- resources/views/components/layout/index.blade.php
+++ resources/views/components/layout/index.blade.php
@@ -1,47 +1,80 @@
-@props(['active' => null])
-
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-
-    <meta name="application-name" content="{{ config('app.name') }}">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>{{ config('app.name') }}</title>
-
-    <link rel="preconnect" href="https://rsms.me/">
-    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
-
-    <style>
-        [x-cloak] {
-            display: none !important;
-        }
-    </style>
-    @livewireStyles
-
-    @filamentStyles
-    @vite('resources/css/app.css')
-</head>
-
-<body class="antialiased min-h-screen">
-<?php
-use Dnw\Foundation\User\UserViewModel;
-
-/** @var UserViewModel $user */ ?>
-@if($user->isAuthenticated)
-<x-nav.index :active="$active"/>
-@endif
-{{ $slot }}
-
-@filamentScripts
-@livewireScripts
-@vite('resources/js/app.js')
-
-@if(config('app.debug'))
-    <script src="https://cdn.jsdelivr.net/gh/underground-works/clockwork-browser@1/dist/toolbar.js"></script>
-    <script src="https://cdn.jsdelivr.net/gh/underground-works/clockwork-browser@1/dist/metrics.js"></script>
-@endif
-</body>
-</html>
+<div id="header">
+    <div id="header-container">
+        <a href="./">
+            <img id="logo" src="images/logo.png" alt="webDiplomacy">
+        </a>
+        <div>
+            <div id="header-welcome">
+                Welcome, <a href="./userprofile.php?userID=20" profilelinkuserid="20">2</a> (90<img
+                    src="images/icons/points.png" alt="D" title="webDiplomacy points"><img src="images/icons/mod.png"
+                                                                                           alt="Mod"
+                                                                                           title="Moderator/Admin"><img
+                    style="" class="userOnlineImg" userid="20" src="images/icons/online.png" alt="Online"
+                    title="User currently logged on">)<span class="userRelationships" profilelinkuserid="20"></span> (<a
+                    href="index.php?auid=20">+</a>) -
+                <span class="logon">(<a href="logon.php?logoff=on" class="light">Log off</a>)</span>
+            </div>
+            <div id="header-goto">
+                <div class="nav-wrap">
+                    <div class="nav-tab"><a href="index.php?" title="See what's happening">Home</a></div>
+                    <div class="nav-tab"><a href="forum.php"
+                                            title="The forum; chat, get help, help others, arrange games, discuss strategies">Forum</a>
+                    </div>
+                    <div id="navSubMenu" class="clickable nav-tab">Search ▼
+                        <div id="nav-drop">
+                            <a href="search.php">Find User</a>
+                            <a href="gamelistings.php?gamelistType=Search">Game Search</a>
+                            <a href="detailedSearch.php" title="advanced search of users and games">Advanced Search</a>
+                        </div>
+                    </div>
+                    <div id="navSubMenu" class="clickable nav-tab">Games ▼
+                        <div id="nav-drop">
+                            <a href="gamelistings.php?gamelistType=New"
+                               title="Game listings; a searchable list of the games on this server">New Games</a>
+                            <a href="gamelistings.php?gamelistType=Open%20Positions"
+                               title="Open positions dropped by other players, free to claim">Open Games</a>
+                            <a href="gamecreate.php" title="Start up a new game">Start a New Game</a>
+                            <a href="botgamecreate.php" title="Start up a new bots-only game">Start an AI/Bot Game</a>
+                            <a href="gamelistings.php?gamelistType=Active"
+                               title="View/Spectate games currently running">Active Games</a>
+                            <!-- <a href="ghostRatings.php" title="Ghost Ratings Information">Ghost Ratings</a> -->
+                            <a href="tournaments.php"
+                               title="Information about tournaments on webDiplomacy">Tournaments</a>
+                            <a href="halloffame.php" title="Information about tournaments on webDiplomacy">Hall of
+                                Fame</a>
+                        </div>
+                    </div>
+                    <div id="navSubMenu" class="clickable nav-tab">Account ▼
+                        <div id="nav-drop">
+                            <a href="usercp.php" title="Change your user specific settings">Site Settings</a>
+                            <a href="group.php" title="Manage your user relationships">User Relationships</a>
+                        </div>
+                    </div>
+                    <div id="navSubMenu" class="clickable nav-tab">Help ▼
+                        <div id="nav-drop">
+                            <a href="rules.php">Site Rules</a>
+                            <a href="faq.php" title="Frequently Asked Questions">FAQ</a>
+                            <a href="intro.php" title="Intro to Diplomacy">Diplomacy Intro</a>
+                            <a href="points.php" title="Points and Scoring Systems">Points/Scoring</a>
+                            <a href="variants.php" title="Active webDiplomacy variants">Variants</a>
+                            <a href="help.php" title="Site information; guides, stats, links">More Info</a>
+                            <a href="modforum.php">Get Help</a>
+                            <a href="donations.php">Donate</a>
+                        </div>
+                    </div>
+                    <div id="navSubMenu" class="clickable nav-tab">Mods ▼
+                        <div id="nav-drop">
+                            <a href="modforum.php" title="The modforum; ask your mod-team for help.">ModForum</a>
+                            <div class="hr"></div>
+                            <a href="admincp.php">Admin CP</a>
+                            <a href="admincp.php?tab=Multi-accounts">Multi Finder</a>
+                            <a href="admincp.php?tab=Groups">User Groups</a>
+                            <a href="admincp.php?tab=Chatlogs">Pull Press</a>
+                            <a href="admincp.php?tab=AccessLog">Access Log</a>
+                            <a href="search.php">Find User</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/modern/components/layout/index.blade.php
@@ -0,0 +1,47 @@
+@props(['active' => null])
+
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+
+    <meta name="application-name" content="{{ config('app.name') }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{ config('app.name') }}</title>
+
+    <link rel="preconnect" href="https://rsms.me/">
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+
+    <style>
+        [x-cloak] {
+            display: none !important;
+        }
+    </style>
+    @livewireStyles
+
+    @filamentStyles
+    @vite('resources/css/app.css')
+</head>
+
+<body class="antialiased min-h-screen">
+<?php
+use Dnw\Foundation\User\UserViewModel;
+
+/** @var UserViewModel $user */ ?>
+@if($user->isAuthenticated)
+<x-nav.index :active="$active"/>
+@endif
+{{ $slot }}
+
+@filamentScripts
+@livewireScripts
+@vite('resources/js/app.js')
+
+@if(config('app.debug'))
+    <script src="https://cdn.jsdelivr.net/gh/underground-works/clockwork-browser@1/dist/toolbar.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/underground-works/clockwork-browser@1/dist/metrics.js"></script>
+@endif
+</body>
+</html>
