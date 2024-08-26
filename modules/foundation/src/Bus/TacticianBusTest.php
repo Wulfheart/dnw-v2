d98da91b380f0dc1ef4d2d1b22e83212eee144f1
Alex Wulf <alex.f.wulf@gmail.com>
2024-08-26 22:54:22 +0200

    Bus tested

---
+++ modules/foundation/src/Bus/TacticianBusTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Dnw\Foundation\Bus;
+
+use League\Tactician\CommandBus;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(TacticianBus::class)]
+class TacticianBusTest extends TestCase
+{
+    public function test_handle(): void
+    {
+        $commandBus = $this->createMock(CommandBus::class);
+
+        $commandBus->method('handle')
+            ->willReturn('result');
+
+        $tacticianBus = new TacticianBus($commandBus);
+
+        $this->assertEquals('result', $tacticianBus->handle('foo'));
+    }
+}
