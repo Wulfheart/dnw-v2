b997737bb37adb0d417f158575896633dfdde26c
Alex Wulf <alex.f.wulf@gmail.com>
2024-11-05 16:38:22 +0100

    Remove internachi/modular

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Providers\AppServiceProvider;
+use App\Providers\ModuleProvider;
 use Dnw\Foundation\Providers\FoundationEventServiceProvider;
 
 return [
     AppServiceProvider::class,
+    ModuleProvider::class,
     FoundationEventServiceProvider::class,
 ];
