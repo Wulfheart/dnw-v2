d9c2e76c609ecfc6a1028efc277b6668cf95ad17
Alex Wulf <alex.f.wulf@gmail.com>
2025-01-28 17:53:53 +0100

    Add tempest/discovery

---
+++ modules/foundation/src/Event/DomainEventProviderTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Dnw\Foundation\Event;
+
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(DomainEventProvider::class)]
+class DomainEventProviderTest extends TestCase {}
